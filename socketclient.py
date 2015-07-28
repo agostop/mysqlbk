2321b37bc097392a62a71b8a3ee7b787ecf204d8
agostop <kingonlyone@gmail.com>
2015-07-28 23:35:38 +0800

    增加了crc校验函数

--- socketclient.py
+++ socketclient.py
@@ -1,15 +1,15 @@
 #!python
 #coding=utf-8
 from socket import *
-import json,os,time,zipfile,sys
+import json,os,time,zipfile,binascii
 
 DEBUG=1
 BUFSIZE=1024
 BAKSERV_IP = "127.0.0.1"
 PORT = 10001
 DATABASE_NAME='skynew'
-BACKPATH='d:\\WLMP\\back_database\\'
-MYSQLDUMP='d:\\WLMP\\MySQL\\bin\\mysqlbak.exe'
+BAKCPATH='d:\\WLMP\\back_database\\'
+MYSQLDUMP='d:\\WLMP\\MySQL\\bin\\'
 #time format "%Y%m%d%H%M%S"
 TIMESTR= time.strftime('%Y%m%d',time.localtime(time.time()))
 
@@ -19,28 +19,40 @@ def debug_log(msg):
 	else:
 		pass
 
+def file_crc32(filename):
+	try:
+		blocksize = 1024 * 64
+		f = open(filename, "rb")
+		str = f.read(blocksize)
+		crc = 0
+		while len(str) != 0:
+			crc = binascii.crc32(str,crc) & 0xffffffff
+			str = f.read(blocksize)
+		f.close()
+
+	except:
+		print "compute file crc failed!"
+		return 0
+
+	return '%08x' % crc
+
 def sendfiledata(filepath,filesize,BUFSIZE,tcpClient):
 	file_size =filesize
 	debug_log('now open the file')
 	mydata = open(filepath, "rb")
 	sendsize=0
 	Flag=True
-	debug_log('sending data...')
 	while Flag:
 		if file_size < sendsize+BUFSIZE:
 			debug_log('send remain data')
 			chunk = mydata.read(file_size - sendsize)
 			Flag = False
 		else:
-			#debug_log('sum is :%d\nsending %d\r' % (sendsize,file_size))
-			sys.stdout.write('sum is :%d  sending %d\r' % (sendsize,file_size))
-			sys.stdout.flush()
 			chunk = mydata.read(BUFSIZE)
 			sendsize+=BUFSIZE
 		tcpClient.sendall(chunk)
 
 	debug_log('data send over')
-			
 	mydata.close()
 	
 	#debug_log('now to send EOF')
@@ -54,13 +66,14 @@ def sendfiledata(filepath,filesize,BUFSIZE,tcpClient):
 	else:
 		return 0
 
+
 def Sendfile(fileinfo):
 	tcpClient = socket(AF_INET,SOCK_STREAM)
-	#tcpClient.settimeout(5)  
+	sock.settimeout(5)  
 	e=0  
-	try: 
-	    tcpClient.connect((BAKSERV_IP,PORT))     
-	except tcpClient.settimeout,e:  
+	try:  
+	    sock.connect((BAKSERV_IP,PORT))     
+	except sock.settimeout,e:  
 	    return 'connect timeout'  
 	except e:  
 	    return 'connect have a error'  
@@ -76,14 +89,17 @@ def Sendfile(fileinfo):
 	debug_log('send file info data')
 	tcpClient.sendall('%s' % data_tosend)
 
-	ready = tcpClient.recv(BUFSIZE).strip()
-	debug_log('rc is |%s|' % ready)
-	if ready == 'comeon':
-		rt=sendfiledata(filepath,filesize,BUFSIZE,tcpClient)
-		tcpClient.close()
+	try:
+		ready = tcpClient.recv(BUFSIZE).strip()
+		debug_log('rc is |%s|' % ready)
+		if ready == 'comeon':
+			rt=sendfiledata(filepath,filesize,BUFSIZE,tcpClient)
+			tcpClient.close()
+		
 		return 1
-	else:
-		return 0
+	except Exception,ex:
+		print Exception,":",ex
+		tcpClient.close()
 
 def sqlbak():
 	if not os.path.exists(BACKPATH):
@@ -93,13 +109,11 @@ def sqlbak():
 	zip_file_name = '%s%s.zip' % (BACKPATH,TIMESTR)
 	debug_log('at %s backup the %s ...' % (TIMESTR,DATABASE_NAME))
 
-	sql_comm='%s --default-character-set=utf8 -hlocalhost -R --triggers -B %s > %s' % (MYSQLDUMP,DATABASE_NAME,backfile)
-	debug_log('sql_comm is :%s'%sql_comm)
+	sql_comm='%smysqlbak --default-character-set=utf8 -hlocalhost -R --triggers -B %s > %s%s.sql' % (MYSQLDUMP,DATABASE_NAME,backfile)
 	if os.system(sql_comm) == 0:
 		debug_log('NOTE: %s is backup successfully' % DATABASE_NAME)
 	else:
-		debug_log('ERROR: %s is backup Failed, sql_comm is exec failed.' % DATABASE_NAME)
-		os._exit(1)
+		debug_log('ERROR: %s is backup Failed.' % DATABASE_NAME)
 
 	if os.path.exists(backfile):
 		f = zipfile.ZipFile(zip_file_name,'w',zipfile.ZIP_DEFLATED)
@@ -108,18 +122,14 @@ def sqlbak():
 		return zip_file_name
 	else:
 		debug_log('ERROR: cannot find the sql back file.')
-		os._exit(1)
+		os._exit()
 
 if __name__ == '__main__':
-	fpath = 'D:\\init_file\\mseinstall.exe'
+	fpath = 'd:\\skyclassSetup\\LiveInfoSetup.exe'
 	#fpath = sqlbak()
-	debug_log('fpath is :%s'%fpath)
-	if fpath:
-		fname = os.path.basename(fpath)
-		fsize = os.stat(fpath).st_size
-		fileinfo = {'filepath':fpath, 'filename':fname, 'filesize':fsize}
-		if not Sendfile(fileinfo):
-			print 'the server not ready'
-	else :
-		os._exit(0)
+	debug_log(file_crc32(fpath))
+#	fname = os.path.basename(fpath)
+#	fsize = os.stat(fpath).st_size
+#	fileinfo = {'filepath':fpath, 'filename':fname, 'filesize':fsize}
+#	ret=Sendfile(fileinfo)
 
